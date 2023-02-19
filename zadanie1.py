80cedc19c86b3ab933faa744fbedf407cf33a8f4
Szymon Misztal <sz.misztal@gmail.com>
2023-02-19 14:10:32 +0100

    first commit

---
+++ zadanie1.py
@@ -0,0 +1,16 @@
+print("Lista zakupów:")
+
+lista_zakupów = {
+    'piekarnia': ['chleb', 'pączek', 'bułki'],
+    'warzywniak': ['marchew', 'seler', 'rukola']
+}
+
+for key, value in lista_zakupów.items():
+    value = str(value)
+    print("Idę do", key.capitalize(), "i kupuję tam", value.title())
+
+ilość_produktów = (len(lista_zakupów['piekarnia'] + lista_zakupów['warzywniak']))
+print("W sumię kupuję", ilość_produktów, "produktów")
+
+print("dcba")
+
